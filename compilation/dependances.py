be1f200f193d48bb7e8422873bf9691389f8aa33
Guillaume_Blot-Teyssedre <guillaume71670@hotmail.fr>
2019-02-17 22:57:54 +0100

    Restructuration de la vérification des dépendances

--- compilation/dependances.py
+++ compilation/dependances.py
@@ -1,39 +1,41 @@
 # -*- coding: utf-8 -*-
 
-try:
-    import os
-except ImportError:
-    print("La bibliothèque python \"os\" n'est pas installée et est nécessaire pour continuer ...")
-    raise
-try:
-    import shutil
-except ImportError:
-    print("La bibliothèque python \"shutil\" n'est pas installée et est nécessaire pour continuer ...")
-    raise
-try:
-    import subprocess
-except ImportError:
-    print("La bibliothèque python \"subprocess\" n'est pas installée et est nécessaire pour continuer ...")
-    raise
-try:
-    import yaml
-except ImportError:
-    print("La bibliothèque python \"yaml\" n'est pas installée et est nécessaire pour continuer ...")
-    raise
-try:
-    import gettext
-except ImportError:
-    print("La bibliothèque python \"gettext\" n'est pas installée et est nécessaire pour continuer ...")
-    raise
-try:
-    import argparse
-except ImportError:
-    print("La bibliothèque python \"argparse\" n'est pas installée et est nécessaire pour continuer ...")
-    raise
-
-programmes_systeme = ["latexmk", "lualatex"]
-for prog in programmes_systeme:
-    if shutil.which(prog) == None:
-        print(prog + " n'est pas installé et est indiscipensable à la compilation.")
-        raise OSError()
-
+def verifier_dependances():
+    satisfaites = True
+    try:
+        import os
+    except ImportError:
+        print("La bibliothèque python \"os\" n'est pas installée et est nécessaire pour continuer.")
+        satisfaites = False
+    try:
+        import shutil
+    except ImportError:
+        print("La bibliothèque python \"shutil\" n'est pas installée et est nécessaire pour continuer.")
+        satisfaites = False
+    try:
+        import subprocess
+    except ImportError:
+        print("La bibliothèque python \"subprocess\" n'est pas installée et est nécessaire pour continuer.")
+        satisfaites = False
+    try:
+        import yaml
+    except ImportError:
+        print("La bibliothèque python \"yaml\" n'est pas installée et est nécessaire pour continuer.")
+        satisfaites = False
+    try:
+        import gettext
+    except ImportError:
+        print("La bibliothèque python \"gettext\" n'est pas installée et est nécessaire pour continuer.")
+        satisfaites = False
+    try:
+        import argparse
+    except ImportError:
+        print("La bibliothèque python \"argparse\" n'est pas installée et est nécessaire pour continuer.")
+        satisfaites = False
+    
+    programmes_systeme = ["latexmk", "lualatex"]
+    for prog in programmes_systeme:
+        if shutil.which(prog) == None:
+            print(prog + " n'est pas installé et est indiscipensable à la compilation.")
+            satisfaites = False
+    return satisfaites
--- compile.py
+++ compile.py
@@ -9,13 +9,15 @@ from compilation import dependances
 from compilation import dossiers
 from compilation import parseur
 
-pars = parseur.Parseur()
-
-if pars.args.compiler:
-    dossiers.compilation()
-if pars.args.nettoyer_tout:
-    dossiers.nettoyage()
-if pars.args.nettoyer:
-    dossiers.nettoyage_integral()
-if not any([pars.args.compiler, pars.args.nettoyer_tout, pars.args.nettoyer]):
-    print("Aucune commande fournie. Utiliser -c pour compiler ou -h pour obtenir de l'aide")
+if dependances.verifier_dependances():
+    pars = parseur.Parseur()
+    if pars.args.compiler:
+        dossiers.compilation()
+    if pars.args.nettoyer_tout:
+        dossiers.nettoyage()
+    if pars.args.nettoyer:
+        dossiers.nettoyage_integral()
+    if not any([pars.args.compiler, pars.args.nettoyer_tout, pars.args.nettoyer]):
+        print("Aucune commande fournie. Utiliser -c pour compiler ou -h pour obtenir de l'aide")
+else:
+    print("Abandon")
