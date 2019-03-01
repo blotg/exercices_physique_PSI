cf18bc315cd288849bd2ada113ef19dfc9c381ef
Guillaume_Blot-Teyssedre <guillaume71670@hotmail.fr>
2019-03-01 14:51:05 +0100

    Ajout de commentaires

--- sources/lua/reponses.lua
+++ sources/lua/reponses.lua
@@ -1,29 +1,29 @@
-Reponses = {chaine = "", tempExercice = "", tempSubsection = "", tempSection = ""}
+Reponses = {chaine = "", tempExercice = "", tempSubsection = "", tempSection = ""}-- Initialisation de l'objet Reponses
 
-function Reponses:reponse(nQuestion, nSubQuestion, reponse)
-    Reponses.chaine = Reponses.chaine .. Reponses.tempSection
-    Reponses.tempSection = ""
-    Reponses.chaine = Reponses.chaine .. Reponses.tempSubsection
-    Reponses.tempSubsection = ""
-    Reponses.chaine = Reponses.chaine .. Reponses.tempExercice
-    Reponses.tempExercice = ""
-    Reponses.chaine = Reponses.chaine .. "\\par\\noindent" .. nQuestion .. ".\\tabto{1.4em}";
-    if nSubQuestion ~= "" then
-      Reponses.chaine = Reponses.chaine .. "\\tabto{1.4em}" .. nSubQuestion .. ")\\tabto{2.7em}";
-    end
-    Reponses.chaine = Reponses.chaine .. reponse;
+function Reponses:section(nSection, titre)-- Ajoute une section
+    Reponses.tempSection = "\\setcounter{section}{" .. nSection .. "}\\addtocounter{section}{-1}\\section{" .. titre .. "}"
 end
 
-function Reponses:exercice(nExercice, titre)
-    Reponses.tempExercice = "\\subsubsection*{Exercice " .. nExercice .. " - " .. titre .. "}"
-end
-
-function Reponses:subsection(nSubsection, titre)
+function Reponses:subsection(nSubsection, titre)-- Ajoute une subsection
     Reponses.tempSubsection = "\\setcounter{subsection}{" .. nSubsection .. "}\\addtocounter{subsection}{-1}\\subsection{" .. titre .. "}"
 end
 
-function Reponses:section(nSection, titre)
-    Reponses.tempSection = "\\setcounter{section}{" .. nSection .. "}\\addtocounter{section}{-1}\\section{" .. titre .. "}"
+function Reponses:exercice(nExercice, titre)-- Ajoute un exercice
+    Reponses.tempExercice = "\\subsubsection*{Exercice " .. nExercice .. " - " .. titre .. "}"
+end
+
+function Reponses:reponse(nQuestion, nSubQuestion, reponse)-- Ajoute une réponse
+    Reponses.chaine = Reponses.chaine .. Reponses.tempSection-- Si c'est la première réponse de la section, on valide l'ajout de la section dans la partie réponses
+    Reponses.tempSection = ""
+    Reponses.chaine = Reponses.chaine .. Reponses.tempSubsection-- Idem pour subsection
+    Reponses.tempSubsection = ""
+    Reponses.chaine = Reponses.chaine .. Reponses.tempExercice-- Idem pour exercice
+    Reponses.tempExercice = ""
+    Reponses.chaine = Reponses.chaine .. "\\par\\noindent" .. nQuestion .. ".\\tabto{1.4em}";-- Numéro de la question
+    if nSubQuestion ~= "" then
+      Reponses.chaine = Reponses.chaine .. "\\tabto{1.4em}" .. nSubQuestion .. ")\\tabto{2.7em}";-- Numéro de la sousquestion, éventuellement
+    end
+    Reponses.chaine = Reponses.chaine .. reponse;-- Ajout de la réponse, finalement
 end
 
 return Reponses
